ea7956a38b90ad3e9454d68be8a74b074938ff99
Mmarcos01 <marie.marcos25@gmail.com>
2021-12-19 13:11:08 -0800

    added videos, imgs, styling

--- aaron-mockup/app.js
+++ aaron-mockup/app.js
@@ -9,30 +9,30 @@ function closeNav() {
 }
 
 var clicked = false;
-
+var pink = "#FF0967"
 
 function changeColor(id, ) {
     
     if(id == "svc") {
-        document.getElementById(id).style.color= "red";
+        document.getElementById(id).style.color= pink;
         document.getElementById("abt").style.color= "white";
         document.getElementById("get").style.color= "white";
         document.getElementById("test").style.color= "white";
     }
     if(id == "abt") {
-        document.getElementById(id).style.color= "red";
+        document.getElementById(id).style.color= pink;
         document.getElementById("svc").style.color= "white";
         document.getElementById("get").style.color= "white";
         document.getElementById("test").style.color= "white";
     }
     if(id == "get") {
-        document.getElementById(id).style.color= "red";
+        document.getElementById(id).style.color= pink;
         document.getElementById("svc").style.color= "white";
         document.getElementById("abt").style.color= "white";
         document.getElementById("test").style.color= "white";
     }
     if(id == "test") {
-        document.getElementById(id).style.color= "red";
+        document.getElementById(id).style.color= pink;
         document.getElementById("svc").style.color= "white";
         document.getElementById("abt").style.color= "white";
         document.getElementById("get").style.color= "white";
